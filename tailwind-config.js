b90aeccb10ae971e4ebdafc71db7b50bc109759f
DevDuulat <duulat.bekm@mail.ru>
2025-09-18 12:07:02 +0600

    Add mobile navbar, Tailwind CSS configuration, and initial venue page
    - Implemented a responsive mobile navbar in `navbar.html` with SVG icons and Tailwind CSS styling.
    - Created a `tailwind-config.js` file to extend Tailwind CSS with custom fonts, colors, and sizes.
    - Added an empty `venues.html` file for future venue content.
    - Created a basic `test.html` file for testing purposes.

---
+++ tailwind-config.js
@@ -0,0 +1,28 @@
+// tailwind-config.js
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', 'ui-sans-serif', 'system-ui'],
+            },
+            fontSize: {
+                h1: ['36px', { lineHeight: '44px', fontWeight: '600' }],
+                h2: ['28px', { lineHeight: '36px', fontWeight: '600' }],
+                h3: ['22px', { lineHeight: '30px', fontWeight: '500' }],
+                body: ['16px', { lineHeight: '24px', fontWeight: '400' }],
+                small: ['14px', { lineHeight: '20px', fontWeight: '400' }],
+            },
+            colors: {
+                base: '#FFFFFF', // фон сайта
+                active: '#D9EBD5', // фон активных элементов
+                card1: '#F1E6BA', // фон карточек 2
+                button: '#EEEEEE', // фон кнопок
+                card2: '#F6F6F6', // фон карточек заведений и специалистов
+                navactive: '#232323', // фон активной кнопки в моб. навбаре
+            },
+            borderRadius: {
+                soft: '25px', // общее скругление
+            },
+        },
+    },
+};
